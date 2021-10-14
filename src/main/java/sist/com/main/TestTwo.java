bd9f89ede9619f3fc1fc368272a574ced5a2a3a1
ehwlfk2 <ehwlfk2@gmail.com>
2021-10-14 21:00:32 +0900

    first commit. github connect eclipse.

---
+++ src/main/java/sist/com/main/TestTwo.java
@@ -0,0 +1,12 @@
+package sist.com.main;
+
+public class TestTwo {
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		
+		test.setA(10);
+		
+		System.out.println(test.getA());
+	}
+}
